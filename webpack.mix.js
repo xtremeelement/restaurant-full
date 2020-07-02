8c8b46f70e1d106ccf5e3ccf6d89a0c4a55d15b0
Brandon Downhour <xtremeelement@gmail.com>
2020-07-01 20:45:24 -0400

    Set up views/layouts

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,6 @@ const mix = require("laravel-mix");
  */
 
 // mix.js('resources/js/app.js', 'public/js')
-mix.sass("resources/sass/app.scss", "public/css");
+mix.sass("resources/sass/app.scss", "public/css").browserSync({
+    proxy: "localhost"
+});
