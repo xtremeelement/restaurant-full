4e40cc899c9e8c44672e252dc2087694a12b3f17
Brandon Downhour <xtremeelement@gmail.com>
2020-07-05 21:53:31 -0400

    created controllers and food item pages

---
+++ app/Http/Controllers/StaticPagesController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StaticPagesController extends Controller
+{
+    //
+    public function home()
+    {
+        return view('home');
+    }
+    public function about()
+    {
+        return view('pages/about');
+    }
+    public function waitlist()
+    {
+        return view('pages/waitlist');
+    }
+    public function offers()
+    {
+        return view('pages/offers');
+    }
+    public function contact()
+    {
+        return view('pages/contact');
+    }
+    public function menu()
+    {
+        return view('menu/index');
+    }
+    public function singleMenu()
+    {
+        return view('menu/single-menu');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,43 +13,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', 'StaticPagesController@home');
 
-Route::get('/menu', function () {
-    return view('menu/index');
-});
-Route::get('/menu/{slug}', function () {
-    return view('menu/single-menu');
-});
-Route::get('/waitlist', function () {
-    return view('pages/waitlist');
-});
-Route::get('/contact', function () {
-    return view('pages/contact');
-});
-Route::get('/about', function () {
-    return view('pages/about');
-});
-Route::get('/offers', function () {
-    return view('pages/offers');
-});
-Route::get('/admin', function () {
-    return view('admin/dashboard');
-});
+Route::get('/menu','StaticPagesController@menu');
+Route::get('/menu/{slug}', 'StaticPagesController@singleMenu');
+Route::get('/waitlist', 'StaticPagesController@waitlist');
+Route::get('/contact', 'StaticPagesController@contact');
+Route::get('/about', 'StaticPagesController@about');
+Route::get('/offers', 'StaticPagesController@offers');
+
+
+Route::get('/admin', 'admin\AdminController@dashboard');
 Route::get('/admin/register', function () {
     return view('admin/register');
 });
 Route::get('/admin/login', function () {
     return view('admin/login');
 });
-Route::get('/admin/food-categories', function () {
-    return view('admin/food-categories/all');
-});
-Route::get('/admin/food-categories/create', function () {
-    return view('admin/food-categories/create');
-});
-Route::get('/admin/food-categories/{id}/edit', function () {
-    return view('admin/food-categories/edit');
-});
+Route::get('/admin/food-categories', 'admin\FoodCategoriesController@index');
+Route::get('/admin/food-categories/create', 'admin\FoodCategoriesController@create');
+Route::get('/admin/food-categories/{id}/edit', 'admin\FoodCategoriesController@edit');
+Route::get('/admin/food-items/{id}/edit', 'admin\FoodItemsController@edit');
+Route::get('/admin/food-items/create', 'admin\FoodItemsController@create');
+Route::get('/admin/food-items', 'admin\FoodItemsController@index');
+
+
+
